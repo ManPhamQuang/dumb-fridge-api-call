1b737cf6a033722892bc3ebd90c160da21a4066b
ManPhamQuang <57248833+ManPhamQuang@users.noreply.github.com>
2020-01-07 11:41:19 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 import requests
-#req = {"query": "query foodInFridge{allFoods{name}}"}
-req = {"query": "query foodInFridge{allFoods(where: { quantity_gt: 0 } ){name,duration,quantity,entryDate,id,image{publicUrlTransformed,filename}}}"}
-r = requests.post('https://dumb-fridge.herokuapp.com/admin/api', json= req, headers= {'Accept': 'application/vnd.cap-collectif.preview+json'})
-#r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-print(r.status_code)
-print(r.json())
+from datetime import datetime
+import pytz
+
+def add_food_to_database(food_name):
+    food = {
+        "banana": "\"5e020f6242f6c58ad78915b6\"",
+        "apple": "\"5e01d71f8e78f787a276ff90\"",
+        "pineapple": "\"5e05b0746e6b65003bc2d67e\"",
+        "mandarin": "\"5e12eb99995b1b003bad23ce\"",
+        "Onion (White)": "\"5e05ad4c6e6b65003bc2d65c\"",
+        "lime": "\"5e05ae306e6b65003bc2d662\"",
+        "tomato": "\"5e05ad106e6b65003bc2d656\"",
+        "watermelon": "\"5e05ae4c6e6b65003bc2d664\"",
+        "pepper": "\"5e05b13a6e6b65003bc2d68a\"",
+        "orange": "asdfs"
+    }
+    foodId = food[food_name]
+    # today = f"\"{datetime.now()}\""
+    today = f"\"{datetime.now().isoformat()}\""
+    url = 'https://dumb-fridge.herokuapp.com/admin/api' 
+    headers = {'Accept': 'application/vnd.cap-collectif.preview+json'} 
+    req = {"query": "mutation addFood{updateFood(id: " + foodId +", data: {quantity: 15, entryDate:" + today + "}){name,quantity,duration,entryDate,id}}"}
+    r = requests.post('https://dumb-fridge.herokuapp.com/admin/api', json= req, headers= {'Accept': 'application/vnd.cap-collectif.preview+json'})
+    if r.status_code == 200:
+        print(r.json())
+add_food_to_database('banana')
